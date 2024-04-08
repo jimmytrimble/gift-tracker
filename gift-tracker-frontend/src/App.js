9c95e20ab70182be3ccb49cca05c302dc004d03a
Will Dallmann <storied.exit0s@icloud.com>
2024-04-08 06:38:50 -1000

    Initial file setup for gift-tracker app

---
+++ gift-tracker-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PROJECT 3 TEST </h1>
+      {/* {Nav_Menu}
+      {Homepage} */}
+    </div>
+  );
+}
+
+export default App;
