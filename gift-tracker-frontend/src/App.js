3eb81f3fcb4af64e47d868e532aed79ffd14a375
Will Dallmann <storied.exit0s@icloud.com>
2024-04-08 09:46:27 -1000

    Added auth0 login and logout buttons

--- gift-tracker-frontend/src/App.js
+++ gift-tracker-frontend/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import Homepage from './Components/Homepage';
+import LoginButton from "./LoginComponents/LoginButton"
+import LogoutButton from "./LoginComponents/LogoutButton"
+
 
 function App() {
   return (
     <div className="App">
       <h1>PROJECT 3 TEST </h1>
+      <LoginButton />
+      <LogoutButton />
+      <Routes>
+        {/* <Route path='/' element={<Homepage />} /> */}
+        {/* <Route path='/profile' element={<Profile />} /> */}
+      </Routes>
+
       {/* {Nav_Menu}
       {Homepage} */}
+
+      {/* Test 2 for frontend branch */}
     </div>
   );
 }
