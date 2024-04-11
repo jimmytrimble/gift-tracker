14263107a4336e84e7df3451d5edc2ba47147394
Will Dallmann <storied.exit0s@icloud.com>
2024-04-10 17:25:40 -1000

    Restored updates to knexfile connection strings

--- gift-tracker-backend/knexfile.js
+++ gift-tracker-backend/knexfile.js
@@ -12,19 +12,8 @@ module.exports = {
     client: 'pg',
 
     // create an environment variable to handle this
-
-    connection: 'postgres://postgres:docker@db:5432/db'
-    //connection: 'postgres://postgres:docker@127.0.0.1:5432/db'
-
-    //connection: 'postgres://postgres:docker@db:5432/db'
-   // connection: 'postgres://postgres:docker@127.0.0.1:5432/db'
-    // connection: {
-    //   host: 'db',
-    //   port: 5433,
-    //   database: 'db',
-    //   user: 'postgres',
-    //   password: 'docker'
-    // }
+    connection: building
+    // connection: seeding
 
   },
 
